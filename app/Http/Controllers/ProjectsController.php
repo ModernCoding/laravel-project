7e6732dcfed2fdad2e5d6d2c0ccd62a066684621
Modern Coding <cormorant.programming@gmail.com>
2019-01-10 14:05:33 +0700

    Auth

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -7,9 +7,14 @@ use App\Project;
 
 class ProjectsController extends Controller
 {
+  public function __construct()
+  {
+    $this -> middleware('auth') -> except(['index','show']);
+  }
+
   public function index()
   {
-    $projects = Project::all();
+    $projects = Project::where('owner_id', auth() -> id()) -> get();
     return view('projects.index', compact('projects'));
   }
 
@@ -31,12 +36,14 @@ class ProjectsController extends Controller
 
   public function store()
   {
-    Project::create(
-      request() -> validate([
-        'title' => 'required | min:3 | max:255',
-        'description' => ['required', 'min:3']
-      ])
-    );
+    $attributes = request() -> validate([
+      'title' => 'required | min:3 | max:255',
+      'description' => ['required', 'min:3']
+    ]);
+
+    $attributes['owner_id'] = auth() -> id();
+
+    Project::create($attributes);
 
     return redirect('/projects');
   }
